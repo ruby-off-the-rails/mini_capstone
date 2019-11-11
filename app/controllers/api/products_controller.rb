456dd391c50b4a71b3ba2606eaee216ffa1bcd4b
brian <riceb53@gmail.com>
2019-11-11 17:54:55 -0600

    restful routes without views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,11 +1,38 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
-    render 'products.json.jb'
+    render 'index.json.jb'
   end
 
-  def first
-    @product = Product.find_by(id: 1)
-    render 'one_product.json.jb'
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @product = Product.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: {message: "Product removed"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
-
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/all_products" => "products#index"
-    get "/first_product" => "products#first"
+    get "/products" => "products#index"
+    # url segment
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#update"
+    post "/products" => "products#create"
   end
 end
