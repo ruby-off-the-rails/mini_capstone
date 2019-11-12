9a13a487e1bf2ab883b400015bbca6f0869de3f3
brian <riceb53@gmail.com>
2019-11-11 20:22:51 -0600

    add model methods to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    !(price >= 10)
+  end
+
+  def tax
+    tax_rate = 0.09
+    price * tax_rate
+  end
+
+  def total
+    price + tax
+  end
 end
