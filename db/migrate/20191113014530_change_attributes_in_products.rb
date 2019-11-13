ec3175669d6c8f2f80723467210cb0baee4b6dcf
brian <riceb53@gmail.com>
2019-11-12 20:10:23 -0600

    modify products

---
+++ db/migrate/20191113014530_change_attributes_in_products.rb
@@ -0,0 +1,8 @@
+class ChangeAttributesInProducts < ActiveRecord::Migration[6.0]
+  def change
+    change_column :products, :description, :text
+    change_column :products, :price, "numeric USING CAST(price AS numeric)"
+    change_column :products, :price, :decimal, precision: 8, scale: 2
+
+  end
+end
