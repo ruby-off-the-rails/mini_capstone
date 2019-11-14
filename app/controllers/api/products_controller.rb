a7c2a941267c2ffe9ceb88c30a429a15af6e0cbd
brian <riceb53@gmail.com>
2019-11-13 18:28:01 -0600

    add happy/sad paths

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -16,18 +16,24 @@ class Api::ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description]
     )
-    render 'show.json.jb'
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.update(
-      name: params[:name],
-      price: params[:price],
-      image_url: params[:image_url],
-      description: params[:description]
-    )
-    render 'show.json.jb'
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
