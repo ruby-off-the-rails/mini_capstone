a3f77c4b2eda85ae875d88860cad8a944600ab88
brian <riceb53@gmail.com>
2019-11-16 12:51:28 -0600

    add suppliers and link to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_014530) do
+ActiveRecord::Schema.define(version: 2019_11_15_030314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,15 @@ ActiveRecord::Schema.define(version: 2019_11_13_014530) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "supplier_id"
   end
 
-
-
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
