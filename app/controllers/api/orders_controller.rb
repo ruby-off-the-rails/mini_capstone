4f9f0d6985730068af5e874f64882e71f1efb614
brian <riceb53@gmail.com>
2019-11-20 18:34:21 -0600

    add authorization

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -5,21 +5,25 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
-    calculated_subtotal =  params[:quantity].to_i * product.price
-    calculated_tax =  calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      calculated_subtotal =  params[:quantity].to_i * product.price
+      calculated_tax =  calculated_subtotal * 0.09
+      calculated_total = calculated_subtotal + calculated_tax
 
-    @order = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total
-    )
-    @order.save!
-    render 'show.json.jb'
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total
+      )
+      @order.save!
+      render 'show.json.jb'
+    else
+      render json: {message: "You cannot do this"}
+    end
   end
 
   def show
